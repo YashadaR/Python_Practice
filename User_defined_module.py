c668b544a02d50695112f2209ded3d9f497c0ff0
YashadaR <yashadaraut77@gmail.com>
2025-03-17 12:18:05 +0530

    "Code revised"

---
+++ User_defined_module.py
@@ -0,0 +1,11 @@
+import New_modules
+
+result1 = New_modules.add(10,20)
+result2 = New_modules.sub(50,20)
+result3 = New_modules.multiplication(10,20)
+result4 = New_modules.division(100,20)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ built_in_module.py
@@ -0,0 +1,5 @@
+import math
+
+print(math.fmod(51,4))
+print(math.e)
+print(math.degrees(45))
